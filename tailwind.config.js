24a49facb7401c755690fc5b8344c21d6f7527f1
yuki <33770374+yukitotsunoda@users.noreply.github.com>
2023-11-16 21:09:06 +1000

    Install tailwind line-clamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/line-clamp')]
+};
