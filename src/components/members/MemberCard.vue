47a89e9fedf043b8808180bd374bf817298d9c2b
yuki <33770374+yukitotsunoda@users.noreply.github.com>
2023-11-16 20:53:08 +1000

    Manipulate member details

--- src/components/members/MemberCard.vue
+++ src/components/members/MemberCard.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="w-full shadow-md m-2 px-6 py-4 rounded-2xl lg:w-[30%]">
     <div class="flex">
-      <div class="w-10 h-10 rounded-full flex justify-center items-center mr-6" :style="initialsColour()">
+      <div class="w-12 h-12 rounded-full flex justify-center items-center mr-6" :style="initialsColour()">
         {{ initials }}
       </div>
       <div class="flex flex-col justify-center items-start">
         <p class="text-sm">
           {{ fullName }}
         </p>
-        <p class="text-xs">{{ member.joinedAt }}</p>
+        <p class="text-xs">
+          Member for {{ joinedFor }}
+        </p>
       </div>
     </div>
-    <div class="text-sm text-left mt-4 text-ellipsis">
+    <div class="text-sm text-left mt-4 line-clamp-2">
       {{ member.lastMessage }}
     </div>
   </div>
@@ -22,6 +24,7 @@ import { Member } from '@/services/members'
 import { mapInitialsToHexColour } from '@/utils/colours'
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import { getInitials } from '@/utils/helper'
+import moment from "moment";
 
 @Component({})
 
@@ -29,12 +32,18 @@ export default class MemberCard extends Vue {
   @Prop()
   public member!: Member
 
-  public get fullName() {
-    return this.member && `${this.member?.firstName} ${this.member?.lastName}`
+  public get fullName(): string {
+    return `${this.member?.firstName} ${this.member?.lastName}`
   }
 
-  public get initials() {
-    return this.member && getInitials(this.member.firstName, this.member.lastName);
+  public get initials(): string {
+    return getInitials(this.member.firstName, this.member.lastName);
+  }
+
+  public get joinedFor(): string {
+    const diff = moment(moment()).diff(this.member.joinedAt);
+    const duration = moment.duration(diff);
+    return `${Math.floor(duration.asDays())} days`;
   }
 
   public initialsColour() {
